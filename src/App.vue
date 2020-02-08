2e1554798e7bfeceb2c0044d7d240533b275f2d3
Gon Gis <gon.eu.1987@gmail.com>
2020-02-08 17:50:30 -0300

    adding bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div id="app">
+  <div id="main-app" class="container">
+    <h4>{{title}}</h4>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-
+  name: 'MainApp',
+  data: function() {
+    return {
+      title: "Appointment List"
+    }
+  }
 }
 </script>
 
