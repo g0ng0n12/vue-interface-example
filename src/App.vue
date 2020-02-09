3d79fab828c5d76dc1465408ea688121215d35c0
Gon Gis <gon.eu.1987@gmail.com>
2020-02-08 23:04:38 -0300

    add appointment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="main-app" class="container">
     <div class="row justify-content-center">
-      <appointment-list v-bind:appointments="appointments" />
+      <add-appointment @add="addItem"/>
+      <appointment-list v-bind:appointments="appointments" @remove="removeItem" @edit="editItem"/>
     </div>
   </div>
 </template>
@@ -9,6 +10,8 @@
 <script>
 import axios from "axios";
 import AppointmentList from "./components/AppointmentList";
+import AddAppointment from "./components/AddAppointment";
+import _ from "lodash"
 
 export default {
   name: 'MainApp',
@@ -20,12 +23,33 @@ export default {
   },
 
   components: {
-    AppointmentList
+    AppointmentList,
+    AddAppointment
   },
 
   mounted() {
     axios.get("./data/appointments.json")
-    .then(response => (this.appointments = response.data));
+    .then(response => (this.appointments = response.data.map(item => {
+      item.aptId = this.aptIndex;
+      this.aptIndex++;
+      return item
+    })));
+  },
+  methods: {
+    removeItem: function(apt){
+      this.appointments = _.without(this.appointments, apt);
+    },
+    editItem: function(id, field, text){
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id
+      });
+      this.appointments[aptIndex][field] = text;
+    },
+    addItem: function(apt){
+      apt.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt)
+    },
   }
 }
 </script>
