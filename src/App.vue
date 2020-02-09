4d1969387c2ded65de983a21761d5fcdcae0722a
Gon Gis <gon.eu.1987@gmail.com>
2020-02-08 23:55:01 -0300

    adding Search Component

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,98 @@
 <template>
-  <div id="main-app" class="container">
-    <div class="row justify-content-center">
-      <add-appointment @add="addItem"/>
-      <appointment-list v-bind:appointments="appointments" @remove="removeItem" @edit="editItem"/>
+    <div id="main-app" class="container">
+        <div class="row justify-content-center">
+            <add-appointment @add="addItem"/>
+            <search-appointment @searchRecords="searchAppointments"
+                                :myKey="filterKey"
+                                :myDir="filterDir"
+                                @requestKey="changeKey"
+                                @requestDir="changeDir"/>
+            <appointment-list v-bind:appointments="filterApts" @remove="removeItem" @edit="editItem"/>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import axios from "axios";
-import AppointmentList from "./components/AppointmentList";
-import AddAppointment from "./components/AddAppointment";
-import _ from "lodash"
+    import axios from "axios";
+    import AppointmentList from "./components/AppointmentList";
+    import AddAppointment from "./components/AddAppointment";
+    import SearchAppointment from "./components/SearchAppointment";
+    import _ from "lodash"
 
-export default {
-  name: 'MainApp',
-  data: function() {
-    return {
-      title: "Appointment List",
-      appointments: []
+    export default {
+        name: 'MainApp',
+        data: function () {
+            return {
+                appointments: [],
+                filterKey: "petName",
+                filterDir: "asc",
+                searchTerms: "",
+                aptIndex: 0,
+            }
+        },
+
+        components: {
+            AppointmentList,
+            AddAppointment,
+            SearchAppointment
+        },
+
+        mounted() {
+            axios.get("./data/appointments.json")
+                .then(response => (this.appointments = response.data.map(item => {
+                    item.aptId = this.aptIndex;
+                    this.aptIndex++;
+                    return item
+                })));
+        },
+        computed: {
+            filterApts: function () {
+                return _.orderBy(
+                    this.searchApts,
+                    item => {
+                        return item[this.filterKey].toLowerCase();
+                    },
+                    this.filterDir
+                )
+            },
+            searchApts: function () {
+                return this.appointments.filter(item => {
+                    return (
+                        item.petName.toLowerCase().match(this.searchTerms.toLocaleLowerCase()) ||
+                        item.ownerName.toLowerCase().match(this.searchTerms.toLocaleLowerCase()) ||
+                        item.aptNotes.toLowerCase().match(this.searchTerms.toLocaleLowerCase())
+                    );
+                })
+            },
+
+        },
+        methods: {
+            searchAppointments: function (terms) {
+                this.searchTerms = terms;
+
+            },
+            removeItem: function (apt) {
+                this.appointments = _.without(this.appointments, apt);
+            },
+            editItem: function (id, field, text) {
+                const aptIndex = _.findIndex(this.appointments, {
+                    aptId: id
+                });
+                this.appointments[aptIndex][field] = text;
+            },
+            addItem: function (apt) {
+                apt.aptId = this.aptIndex;
+                this.aptIndex++;
+                this.appointments.push(apt)
+            },
+            changeKey: function (value) {
+                this.filterKey = value;
+            },
+            changeDir: function (value) {
+                this.filterDir = value
+            },
+        }
     }
-  },
-
-  components: {
-    AppointmentList,
-    AddAppointment
-  },
-
-  mounted() {
-    axios.get("./data/appointments.json")
-    .then(response => (this.appointments = response.data.map(item => {
-      item.aptId = this.aptIndex;
-      this.aptIndex++;
-      return item
-    })));
-  },
-  methods: {
-    removeItem: function(apt){
-      this.appointments = _.without(this.appointments, apt);
-    },
-    editItem: function(id, field, text){
-      const aptIndex = _.findIndex(this.appointments, {
-        aptId: id
-      });
-      this.appointments[aptIndex][field] = text;
-    },
-    addItem: function(apt){
-      apt.aptId = this.aptIndex;
-      this.aptIndex++;
-      this.appointments.push(apt)
-    },
-  }
-}
 </script>
 
 <style>
